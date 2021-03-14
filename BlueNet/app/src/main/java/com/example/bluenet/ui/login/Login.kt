1cfbca242865ccd0b6dfd32908a05c0f0214acc5
Kai Min <kae97x@gmail.com>
2021-03-15 00:00:20 +0800

    Create register page

--- BlueNet/app/src/main/java/com/example/bluenet/ui/login/Login.kt
+++ BlueNet/app/src/main/java/com/example/bluenet/ui/login/Login.kt
@@ -1,11 +1,20 @@
 package com.example.bluenet.ui.login
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bluenet.R
+import com.example.bluenet.ui.home.HomeViewModel
+import com.example.bluenet.ui.register.Register
+import java.io.File
+import java.util.*
 
 class Login : AppCompatActivity() {
 
@@ -15,5 +24,40 @@ class Login : AppCompatActivity() {
 
     }
 
-    fun toRegister(view: View) {}
+    // Go register page
+    fun toRegister(view: View) {
+        val intent = Intent(this, Register::class.java)
+        startActivity(intent)
+    }
+
+    fun login(view: View) {
+        var error = ""
+        // Check username & password with database (file for now)
+        try{
+            val account = Scanner(openFileInput("account.txt"))
+            // Get username & password from FILE/DATABASE
+            val dbUsername = SpannableStringBuilder(account.nextLine())
+            val dbPassword = SpannableStringBuilder(account.nextLine())
+
+            // Get Username & Password inputs
+            val inputUsername = findViewById<TextView>(R.id.username).text
+            val inputPassword = findViewById<TextView>(R.id.password).text
+
+            if (inputUsername != dbUsername || inputPassword != dbPassword){
+                error = "Invalid Username/Password!"
+            }
+
+            // Maybe can make a banner instead
+            if (error != ""){
+                Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this, HomeViewModel::class.java)
+                startActivity(intent)
+            }
+
+        } catch (e: Exception){
+        }
+    }
+
 }
---
+++ BlueNet/app/src/main/java/com/example/bluenet/ui/register/Register.kt
@@ -0,0 +1,56 @@
+package com.example.bluenet.ui.register
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.example.bluenet.R
+import com.example.bluenet.ui.home.HomeViewModel
+import java.io.PrintStream
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    fun register(view: View) {
+        // initialise error list
+        var error = ArrayList<String>()
+
+        // Get Username & Password inputs
+        val inputUsername = findViewById<TextView>(R.id.username).text
+        val inputPassword = findViewById<TextView>(R.id.password).text
+        val inputConfirmPassword = findViewById<TextView>(R.id.confirmPassword).text
+        val inputEmail = findViewById<TextView>(R.id.email).text
+
+        // Check if Username exists in DB
+
+        // Check if Password & Confirm Password is the same
+        if (inputPassword != inputConfirmPassword){
+            error.add("Confirm Password is not the same as Password")
+        }
+
+        // Check if email is valid ?
+
+        // Check if there's errors
+        if (error.isNotEmpty()){
+            // Create Account
+            val account = PrintStream(openFileOutput("account.txt", MODE_PRIVATE))
+            account.println(inputUsername)
+            account.println(inputPassword)
+            account.println(inputEmail)
+
+            // Go to Homepage
+            Toast.makeText(this, "Registration Successful!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, HomeViewModel::class.java)
+            startActivity(intent)
+
+        } else{
+            // Show First Error
+            Toast.makeText(this, error[0], Toast.LENGTH_SHORT).show()
+        }
+    }
+}
