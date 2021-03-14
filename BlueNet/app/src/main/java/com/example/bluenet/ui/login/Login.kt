1b436f1279fe06e3489d4daa339c1d16fc7503ef
Kai Min <kae97x@gmail.com>
2021-03-14 21:00:57 +0800

    Create login page

---
+++ BlueNet/app/src/main/java/com/example/bluenet/ui/login/Login.kt
@@ -0,0 +1,19 @@
+package com.example.bluenet.ui.login
+
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bluenet.R
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+    fun toRegister(view: View) {}
+}
