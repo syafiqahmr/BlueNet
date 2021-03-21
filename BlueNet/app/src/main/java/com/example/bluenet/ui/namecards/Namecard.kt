80910974e252a89490c8111087d6a03f5194ad4e
syafiqahmr <syafiqahmr.2018@sis.smu.edu.sg>
2021-03-21 22:48:14 +0800

    Connect Namecards to DB

--- BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/Namecard.kt
+++ BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/Namecard.kt
@@ -4,4 +4,7 @@ import android.media.Image
 
 class Namecard(val id: String, val name: String, val company: String, val image: Int?, val industry: String, val role: String){
 
+    constructor() : this("", "", "", 0, "", ""){
+
+    }
 }
