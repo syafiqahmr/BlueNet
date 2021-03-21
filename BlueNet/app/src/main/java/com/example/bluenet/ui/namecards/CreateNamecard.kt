a49d8fd6c254416b5bef4468e63381abeae1e656
Kai Min <kae97x@gmail.com>
2021-03-21 23:12:27 +0800

    Edit MyNamecardFragment page

--- BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/CreateNamecard.kt
+++ BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/CreateNamecard.kt
@@ -1,12 +1,77 @@
 package com.example.bluenet.ui.namecards
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.annotation.RequiresApi
+import com.example.assignment3.ImagePopup
 import com.example.bluenet.R
+import java.io.File
 
 class CreateNamecard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_namecard)
+
+        initialiseSpinner()
+    }
+
+    companion object {
+        private const val IMAGE_PICK_CODE = 900
+    }
+
+    private fun initialiseSpinner(){
+        val spinnerRole = findViewById<Spinner>(R.id.roleSpinner)
+
+        // Initialise role spinner
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.roles,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerRole?.adapter = adapter
+        }
+
+    }
+
+    fun createOnClick(view: View) {
+        val name = findViewById<EditText>(R.id.name).text.toString()
+        val company = findViewById<EditText>(R.id.company).text.toString()
+
+        if (name != "" && company != ""){
+
+        } else if (name == ""){
+            Toast.makeText(this, "Please Enter Your Name!", Toast.LENGTH_SHORT).show()
+        }  else if (name == ""){
+            Toast.makeText(this, "Please Enter Your Name!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun imageButtonOnClick(view: View) {
+        // go to imagePopup page
+        val intent = Intent(this, ImagePopup::class.java)
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            // handle response after returning from imagePopup page
+            // bind image button with selected image
+            val file = File(getExternalFilesDir(null), "ProfileImageFile.jpg")
+            val uri = Uri.fromFile(file)
+            findViewById<ImageButton>(R.id.namecardPhoto).setImageURI(uri)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
+
--- BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/MyNamecardFragment.kt
+++ BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/MyNamecardFragment.kt
@@ -1,15 +1,28 @@
 package com.example.bluenet.ui.namecards
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.example.assignment3.ImagePopup
 import com.example.bluenet.R
 import com.example.bluenet.databinding.FragmentMyNamecardBinding
 import com.google.firebase.database.FirebaseDatabase
+import java.io.File
+import java.io.PrintStream
+import java.util.*
 
 class MyNamecardFragment : Fragment() {
 
@@ -21,7 +34,12 @@ class MyNamecardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        initialiseSpinner()
         return inflater.inflate(R.layout.fragment_my_namecard, container, false)
+    }
+
+    companion object {
+        private const val IMAGE_PICK_CODE = 900
     }
 
 
@@ -35,7 +53,7 @@ class MyNamecardFragment : Fragment() {
 
         // set button event listener
         val saveButton = fragmentMyNamecardBinding.buttonSave
-        saveButton.setOnClickListener(){
+        saveButton.setOnClickListener {
             saveNamecard()
         }
 
@@ -43,31 +61,75 @@ class MyNamecardFragment : Fragment() {
 
     }
 
+    private fun initialiseSpinner(){
+        val spinnerRole = fragmentMyNamecardBinding.spinnerRole
+
+        this.activity?.let {
+
+            // Initialise role spinner
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.roles,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                spinnerRole.adapter = adapter
+            }
+        }
+
+    }
+
+    fun imageButtonOnClick(view: View) {
+        // go to imagePopup page
+        val intent = Intent(this, ImagePopup::class.java)
+        startActivityForResult(intent, MyNamecardFragment.IMAGE_PICK_CODE)
+    }
+
     private fun saveNamecard(){
 
         Log.d("saved", "clicked!")
 
-        val name = fragmentMyNamecardBinding.name.text.toString()
-        val industry = fragmentMyNamecardBinding.industry.text.toString()
-        val company = fragmentMyNamecardBinding.company.text.toString()
-        val position = fragmentMyNamecardBinding.position.text.toString()
-        val image = fragmentMyNamecardBinding.imageView5
+        val name = fragmentMyNamecardBinding.name.text.toString().trim()
+        val industry = fragmentMyNamecardBinding.industry.text.toString().trim()
+        val company = fragmentMyNamecardBinding.company.text.toString().trim()
+        val position = fragmentMyNamecardBinding.spinnerRole.toString().trim()
+        val image = fragmentMyNamecardBinding.namecardPhoto
 
         // TODO: set input checks
 
-        // TODO: figure out how to send image
+        if (name != "" && company != ""){
+            val ref = FirebaseDatabase.getInstance().getReference("namecards")
+            val namecardId = ref.push().key.toString()
 
-        val ref = FirebaseDatabase.getInstance().getReference("namecards")
-        val namecardId = ref.push().key.toString()
+            val namecard = Namecard(namecardId, name, company, null, industry, position)
 
-        val namecard = Namecard(namecardId, name, company, null, industry, position)
-        
-        ref.child(namecardId).setValue(namecard).addOnCompleteListener(){
-            Toast.makeText(this.activity, "Saved!", Toast.LENGTH_SHORT).show()
-            Log.d("namecardId", namecardId)
+            ref.child(namecardId).setValue(namecard).addOnCompleteListener {
+                Toast.makeText(this.activity, "Saved!", Toast.LENGTH_SHORT).show()
+                Log.d("namecardId", namecardId)
+            }
+        } else if (name == ""){
+            fragmentMyNamecardBinding.name.error = "Name is required!"
+            fragmentMyNamecardBinding.name.requestFocus()
+        }  else if (company == ""){
+            fragmentMyNamecardBinding.company.error = "Name is required!"
+            fragmentMyNamecardBinding.company.requestFocus()
         }
 
-
     }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            // handle response after returning from imagePopup page
+            // bind image button with selected image
+            val file = File(this.activity?.getExternalFilesDir(null), "ProfileImageFile.jpg")
+            val uri = Uri.fromFile(file)
+            fragmentMyNamecardBinding.namecardPhoto.setImageURI(uri)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 
 }
