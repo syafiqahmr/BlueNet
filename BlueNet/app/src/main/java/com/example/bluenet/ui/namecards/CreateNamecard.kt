16a0dc78cb82be5a696553369bf1a6152015cfdb
Kai Min <kae97x@gmail.com>
2021-03-22 19:08:02 +0800

    Fix image uploading

--- BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/CreateNamecard.kt
+++ BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/CreateNamecard.kt
@@ -1,29 +1,34 @@
 package com.example.bluenet.ui.namecards
 
-import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
-import android.net.Uri
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
-import com.example.assignment3.ImagePopup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bluenet.MainActivity
 import com.example.bluenet.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
-import java.io.File
+
 
 class CreateNamecard() : AppCompatActivity() {
 
     private var role = "Entrepreneur"
+    private lateinit var imageBtn: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_namecard)
+        imageBtn = findViewById<ImageButton>(R.id.namecardPhoto)
 
         initialiseSpinner()
     }
@@ -37,9 +42,9 @@ class CreateNamecard() : AppCompatActivity() {
 
         // Initialise role spinner
         ArrayAdapter.createFromResource(
-            this,
-            R.array.roles,
-            android.R.layout.simple_spinner_item
+                this,
+                R.array.roles,
+                android.R.layout.simple_spinner_item
         ).also { adapter ->
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -61,56 +66,65 @@ class CreateNamecard() : AppCompatActivity() {
 
 
     fun imageButtonOnClick(view: View) {
-        // go to imagePopup page
-        val intent = Intent(this, ImagePopup::class.java)
-        startActivityForResult(intent, IMAGE_PICK_CODE)
+
+        val options = arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Choose your profile picture")
+
+        builder.setItems(options, DialogInterface.OnClickListener { dialog, item ->
+            if (options[item] == "Take Photo") {
+                val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(takePicture, 0)
+            } else if (options[item] == "Choose from Gallery") {
+                val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(pickPhoto, 1)
+            } else if (options[item] == "Cancel") {
+                dialog.dismiss()
+            }
+        })
+        builder.show()
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            // handle response after returning from imagePopup page
-            // bind image button with selected image
-            val file = File(getExternalFilesDir(null), "ProfileImageFile.jpg")
-            val uri = Uri.fromFile(file)
-            findViewById<ImageButton>(R.id.namecardPhoto).setImageURI(uri)
+
+        if (resultCode != RESULT_CANCELED) {
+            when (requestCode) {
+                0 -> if (resultCode == RESULT_OK && data != null) {
+                    val selectedImage = data.extras!!["data"] as Bitmap?
+                    imageBtn.setImageBitmap(selectedImage)
+                }
+                1 -> if (resultCode == RESULT_OK && data != null) {
+                    val selectedImage = data.data
+                    val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+                    if (selectedImage != null) {
+                        val cursor: Cursor? = contentResolver.query(selectedImage,
+                                filePathColumn, null, null, null)
+                        if (cursor != null) {
+                            cursor.moveToFirst()
+                            val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
+                            val picturePath: String = cursor.getString(columnIndex)
+                            imageBtn.setImageBitmap(BitmapFactory.decodeFile(picturePath))
+                            cursor.close()
+                        }
+                    }
+                }
+            }
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
 
-    fun createOnClick(view: View) {
-
-        val name = findViewById<EditText>(R.id.name).text.toString().trim()
-        val industry = findViewById<EditText>(R.id.industry).text.toString().trim()
-        val company = findViewById<EditText>(R.id.company).text.toString().trim()
-        val image = findViewById<ImageButton>(R.id.namecardPhoto)
-
-        if (name == "") {
-            findViewById<EditText>(R.id.name).error = "Name is required!"
-            findViewById<EditText>(R.id.name).requestFocus()
-        } else if (company == "") {
-            findViewById<EditText>(R.id.company).error = "Name is required!"
-            findViewById<EditText>(R.id.company).requestFocus()
-        } else if (name != "" && company != "") {
-            val namecard = Namecard(name, company, null, industry, role)
-            saveToDb(namecard)
-
-
-            Log.d("saved", "clicked!")
-
-            val it = Intent(this, MainActivity::class.java)
-            startActivity(it)
-
-        }
-    }
 
     private fun saveToDb(namecard: Namecard){
         val user = FirebaseAuth.getInstance().currentUser
         val ref = FirebaseDatabase.getInstance().getReference("namecards")
 
         // namecard id == uid
-        ref.child(user.uid).setValue(namecard).addOnCompleteListener {
-            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show()
+        if (user != null) {
+            ref.child(user.uid).setValue(namecard).addOnCompleteListener {
+                Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
--- BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/MyNamecardFragment.kt
+++ BlueNet/app/src/main/java/com/example/bluenet/ui/namecards/MyNamecardFragment.kt
@@ -1,23 +1,24 @@
 package com.example.bluenet.ui.namecards
 
 import android.app.Activity
-import android.content.Context
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.text.Editable
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.appcompat.app.AppCompatActivity
 import androidx.annotation.RequiresApi
-import androidx.core.view.get
-import com.example.assignment3.ImagePopup
-import com.example.bluenet.MainActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bluenet.R
 import com.example.bluenet.databinding.FragmentMyNamecardBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -26,7 +27,6 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import java.io.File
-import java.io.PrintStream
 import java.util.*
 
 class MyNamecardFragment : Fragment() {
@@ -36,6 +36,7 @@ class MyNamecardFragment : Fragment() {
 
     private var role = "Entrepreneur"
     private lateinit var user: FirebaseUser
+    private lateinit var imageBtn: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,6 +54,7 @@ class MyNamecardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         fragmentMyNamecardBinding = FragmentMyNamecardBinding.bind(view)
+        imageBtn = fragmentMyNamecardBinding.namecardPhoto
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -102,7 +104,7 @@ class MyNamecardFragment : Fragment() {
     }
 
     private fun getData(){
-        user = FirebaseAuth.getInstance().currentUser
+        user = FirebaseAuth.getInstance().currentUser!!
         val ref = Firebase.database.reference
 
         ref.child("namecards").child(user.uid).get().addOnSuccessListener {
@@ -129,9 +131,23 @@ class MyNamecardFragment : Fragment() {
     }
 
     fun imageButtonOnClick(view: View) {
-        // go to imagePopup page
-        val intent = Intent(this.activity, ImagePopup::class.java)
-        startActivityForResult(intent, MyNamecardFragment.IMAGE_PICK_CODE)
+        val options = arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this.activity)
+        builder.setTitle("Choose your profile picture")
+
+        builder.setItems(options, DialogInterface.OnClickListener { dialog, item ->
+            if (options[item] == "Take Photo") {
+                val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(takePicture, 0)
+            } else if (options[item] == "Choose from Gallery") {
+                val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(pickPhoto, 1)
+            } else if (options[item] == "Cancel") {
+                dialog.dismiss()
+            }
+        })
+        builder.show()
     }
 
     private fun saveNamecard(){
@@ -164,12 +180,29 @@ class MyNamecardFragment : Fragment() {
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            // handle response after returning from imagePopup page
-            // bind image button with selected image
-            val file = File(this.activity?.getExternalFilesDir(null), "ProfileImageFile.jpg")
-            val uri = Uri.fromFile(file)
-            fragmentMyNamecardBinding.namecardPhoto.setImageURI(uri)
+
+        if (resultCode != AppCompatActivity.RESULT_CANCELED) {
+            when (requestCode) {
+                0 -> if (resultCode == AppCompatActivity.RESULT_OK && data != null) {
+                    val selectedImage = data.extras!!["data"] as Bitmap?
+                    imageBtn.setImageBitmap(selectedImage)
+                }
+                1 -> if (resultCode == AppCompatActivity.RESULT_OK && data != null) {
+                    val selectedImage = data.data
+                    val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+                    if (selectedImage != null) {
+                        val cursor: Cursor? = this.requireActivity().contentResolver.query(selectedImage,
+                                filePathColumn, null, null, null)
+                        if (cursor != null) {
+                            cursor.moveToFirst()
+                            val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
+                            val picturePath: String = cursor.getString(columnIndex)
+                            imageBtn.setImageBitmap(BitmapFactory.decodeFile(picturePath))
+                            cursor.close()
+                        }
+                    }
+                }
+            }
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
