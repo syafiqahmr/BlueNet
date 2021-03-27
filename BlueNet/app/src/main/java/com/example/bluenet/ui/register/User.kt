a01276a8c63c8aac7764827ff969ec4dff96a928
Daniel Tan <50669678+danieltan2018@users.noreply.github.com>
2021-03-27 15:59:48 +0800

    Add beacon ID

--- BlueNet/app/src/main/java/com/example/bluenet/ui/register/User.kt
+++ BlueNet/app/src/main/java/com/example/bluenet/ui/register/User.kt
@@ -1,10 +1,8 @@
 package com.example.bluenet.ui.register
 
-class User(val type:String, val boothCode:String){
+class User(val type: String, val boothCode: String, val beaconID: String) {
 
-    constructor() : this("", ""){
-
-    }
+    constructor() : this("", "", "")
 
 
 }
